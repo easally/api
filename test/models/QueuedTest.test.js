1c8c97c264d292093b8cade8917655974eac0db0
Stephen Lewis <code@stephenlewis.me>
2019-10-05 12:50:59 +0100

    Populate QueuedTest status when creating

--- test/models/QueuedTest.test.js
+++ test/models/QueuedTest.test.js
@@ -1,23 +1,33 @@
 'use strict'
 
-const QueuedTest = require('./../../lib/models/QueuedTest')
 const Lab = require('@hapi/lab')
 const { expect } = require('@hapi/code')
-const { describe, it } = exports.lab = Lab.script()
+const { beforeEach, describe, it } = exports.lab = Lab.script()
+
+const QueuedTest = require('./../../lib/models/QueuedTest')
+const status = require('./../../lib/constants/testStatus')
 
 describe('QueuedTest', () => {
-  it('generates the queued_at property', async () => {
-    const model = new QueuedTest()
+  let model
 
+  beforeEach(() => {
+    model = new QueuedTest()
+  })
+
+  it('generates the queued_at property', async () => {
     await model.$beforeInsert(null)
 
     expect(model.queued_at).to.be.date()
   })
 
+  it('generates the status property', async () => {
+    await model.$beforeInsert(null)
+
+    expect(model.status).to.equal(status.QUEUED)
+  })
+
   // Smoke-test, to ensure the parent method is called
   it('generates the uuid property', async () => {
-    const model = new QueuedTest()
-
     await model.$beforeInsert(null)
 
     expect(model.uuid).to.not.be.null()
