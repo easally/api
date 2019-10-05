55aa647a611d8253c612253d8d57fefb09ff93e2
Stephen Lewis <code@stephenlewis.me>
2019-10-05 11:43:55 +0100

    Initialise storage layer (Knex / Objection)

---
+++ knexfile.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+
+module.exports = {
+  client: 'mysql2',
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD
+  },
+  migrations: {
+    directory: './database/migrations',
+    tableName: 'knex_migrations'
+  }
+}
