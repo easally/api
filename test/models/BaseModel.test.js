5ac28a0f6600a38fdfa14e4bc1c1a4d833842ad6
Stephen Lewis <code@stephenlewis.me>
2019-10-05 12:04:17 +0100

    Create BaseModel, which generates a UUID

---
+++ test/models/BaseModel.test.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const BaseModel = require('./../../lib/models/BaseModel')
+const Lab = require('@hapi/lab')
+const { expect } = require('@hapi/code')
+const { describe, it } = exports.lab = Lab.script()
+
+describe('BaseModel', () => {
+  it('generates a UUID', async () => {
+    const pattern = new RegExp('^[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}$', 'i')
+    const model = new BaseModel()
+
+    await model.$beforeInsert(null)
+
+    expect(pattern.test(model.uuid)).to.be.true()
+  })
+})
