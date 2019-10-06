0584ac99ff16d014d2233c7616a9f506452dbcaf
Stephen Lewis <code@stephenlewis.me>
2019-10-06 16:28:46 +0100

    Tidy up server.js

--- app/server.js
+++ app/server.js
@@ -1,15 +1,27 @@
 'use strict'
 
 const Hapi = require('@hapi/hapi')
-const Knex = require('knex')
-const knexConfig = require('./../knexfile')
+const Knex = require('knex')(require('./../knexfile'))
 const { Model } = require('objection')
+
+// -----------------------------------------------------------------------------
+// Create the server instance
+// -----------------------------------------------------------------------------
 const server = Hapi.server({ host: 'localhost', port: 3000 })
 
+// -----------------------------------------------------------------------------
 // Load the environment variables
+// -----------------------------------------------------------------------------
 require('./../config/env').init()
 
+// -----------------------------------------------------------------------------
+// Initialise the ORM
+// -----------------------------------------------------------------------------
+Model.knex(Knex)
+
+// -----------------------------------------------------------------------------
 // Register the routes
+// -----------------------------------------------------------------------------
 const routes = [
   ...require('../routes/home'),
   ...require('../routes/tests')
@@ -17,16 +29,17 @@ const routes = [
 
 routes.forEach(route => server.route(route))
 
-// Initialise the database
-Model.knex(Knex(knexConfig))
-
+// -----------------------------------------------------------------------------
 // Initialise the server
+// -----------------------------------------------------------------------------
 exports.init = async () => {
   await server.initialize()
   return server
 }
 
+// -----------------------------------------------------------------------------
 // Start the server
+// -----------------------------------------------------------------------------
 exports.start = async () => {
   await server.start()
   console.log(`Server running at ${server.info.uri}`)
